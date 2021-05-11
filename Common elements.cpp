0f6faedc83211809c53912eeb18804dab607e5ff
Ari7l <65645372+Ari7l@users.noreply.github.com>
2021-05-11 16:09:18 +0530

    Add files via upload
    Common elements gfg
    Find all unique element present in all three array

---
+++ Common elements.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+    public:    
+       vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
+        {
+            vector<int>res;
+            unordered_map<int,int>mp1;
+            unordered_map<int,int>mp2;
+           for(int i=0;i<n1 ;i++){
+               if(mp1[A[i]]) continue;
+               mp1[A[i]]++;
+           }
+           for(int i=0;i<n2 ;i++){
+               if(mp2[B[i]]) continue;
+               mp2[B[i]]++;
+           }
+           int ele=0;
+           for(int i=0 ;i<n3 ;++i){
+               if(mp1[C[i]] && mp2[C[i]]){ //If the element exists in both A and B 
+                  if(ele != C[i]){ // Duplicate element must inserted only once
+                       res.push_back(C[i]);
+                       ele = C[i];
+                  }
+                       
+               }
+           }
+           return res;
+            //code here.
+        }
+
+};
