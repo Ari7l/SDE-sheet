7ec0c08835d6d13fc8772fc93d4a4e4e13e1c868
Ari7l <65645372+Ari7l@users.noreply.github.com>
2021-05-11 15:24:25 +0530

    Update Count pairs with given sum.cpp

--- Count pairs with given sum.cpp
+++ Count pairs with given sum.cpp
@@ -7,7 +7,7 @@ public:
         for(int i=0;i<n;++i) mp[arr[i]]++; // conunting the frequency of each element
         for(int i=0;i<n;++i){
             int rem = k - arr[i]; 
-            Pairs+=mp[rem]; // if there is a element in array such that there sum == k then increment the count
+            Pairs+=mp[rem]; // if there is a element in array such that their sum == k then increment the count
             if(rem==arr[i])Pairs--;//(1,2) and (2,1) are same hence decrement count
         }
         return Pairs/2; // ew have counted the frequeceny twice hence take the half 
