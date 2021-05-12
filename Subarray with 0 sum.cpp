cb722d7c17ec7118ed51da017f6eb3a06bc697eb
Ari7l <65645372+Ari7l@users.noreply.github.com>
2021-05-12 23:53:59 +0530

    Add files via upload
    Subarray with 0 sum from gfg

---
+++ Subarray with 0 sum.cpp
@@ -0,0 +1,27 @@
+//Subarray with 0 sum
+class Solution{
+    public:
+    //Function to check whether there is a subarray present with 0-sum or not.
+    bool subArrayExists(int arr[], int n)
+    {  
+       unordered_set<int>st; //O(1) operation O(N) space
+       int sum =0;
+       //The idea is to find the prefix  sum till ith index and check if that sum already exist or 
+       // th sum is '0' if either of the condition works the array contains the subarray with sum 0
+       // This works because if the prefix sum repeats that means the elements between the till 
+       // repeated sum must add upto zero 
+       //e.g. 4 2 -3 1 6 
+       // sum 4 6  3 4  now 4 repeats it means the elements till another 4 add upto zero 2 -3 1 
+       for(int i=0;i<n;++i){
+           sum+=arr[i];
+           if(st.find(sum)!=st.end()||sum==0){
+               return true;
+           }
+           st.insert(sum);
+       }
+       return false;
+       }
+      
+      // TIME and SPACE : O(N)  
+
+};
