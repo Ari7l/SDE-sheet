dedb0a93e499f1f94f0869464310267512fd8780
Ari7l <65645372+Ari7l@users.noreply.github.com>
2021-05-13 17:49:30 +0530

    Update gfg-Longest consecutive subsequence.cpp

--- gfg-Longest consecutive subsequence.cpp
+++ gfg-Longest consecutive subsequence.cpp
@@ -13,7 +13,7 @@ class Solution{
        for(int i=0 ;i<N;++i){
            //e.g [1,2,6,3,5,2,2,1,4]
           // pick a random element each time, and count the elements that you can "go left" and "go right" with
-          // so suppose you picked 3 randomly from the hashmap in the example above
+          // so suppose you picked 3 randomly from the set in the example above
           //then check 2 and 4 next. then check 1 and 5 next. then check 6 because 0 doesn't exist in the array.
           //Bam. now you have your answer (right-left-1) 
           int right= arr[i]+1;
