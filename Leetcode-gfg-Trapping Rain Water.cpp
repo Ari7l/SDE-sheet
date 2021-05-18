6ac928e83e2e70c82c00ef877bd8456d04594990
Ari7l <65645372+Ari7l@users.noreply.github.com>
2021-05-18 13:41:47 +0530

    Update Leetcode-gfg-Trapping Rain Water.cpp

--- Leetcode-gfg-Trapping Rain Water.cpp
+++ Leetcode-gfg-Trapping Rain Water.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     //arr[left] or arr[right] always is a pointer to a value which is the max value found so far, searching from left and right.
 
-//If arr[left] <= arr[right], this means that arr[right] is the max value found so far. This also means that maxLeft will be less than or equal to arr[right], which means that maxLeft will be boundry to the answer(res variable, in this solution), since it has be less than or equal to arr[right] e, so we don't have to consider maxRight when adding to the counter.
+//If arr[left] <= arr[right], this means that arr[right] is the max value found so far. This also means that maxLeft will be less than or equal to arr[right], 
+ //which means that maxLeft will be boundry to the answer(res variable, in this solution), 
+    //since it has be less than or equal to arr[right] e, so we don't have to consider maxRight when adding to the counter.
 
 //If arr[left] > arr[right] , this means at arr[left] is the max value found so far. Same thing applies as explained in the paragraph above
 // so basically which ever height is smaller that will be the boundry as water cant be contained above it instead of calculating the trueMaxRight and trueMaxLeft we just compare the max height from left and right 
