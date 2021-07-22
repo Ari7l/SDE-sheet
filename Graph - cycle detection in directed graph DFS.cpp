77121ab06cc196b7efec9caa805cb72a95f39d53
Ari7l <65645372+Ari7l@users.noreply.github.com>
2021-07-22 17:44:47 +0530

    Add files via upload

---
+++ Graph - cycle detection in directed graph DFS.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+bool cycleDfs(int src, vector<int> adj[], int vis[],int dfsVis[]) {
+    vis[src]=1;
+    dfsVis[src]=1;
+     for(auto it : adj[src]) {
+        if(vis[it] == 0) {
+            if(bipartiteDfs(it, adj, vis,dfsVis)) return true;
+            }
+         else if(dfsVis[it]==1) return true;
+        
+        }
+     dfsVis[it]=0;
+        
+    return false; 
+}
+bool cycleDetect(vector<int> adj[], int n) {
+    int vis[n];
+    int dfsVis[n];
+    memset(vis, 0, sizeof color); 
+    memset(vis, 0, sizeof color); 
+    for(int i = 0;i<n;i++) {
+        if(vis[i]==0){
+            if(cycleDfs(i, adj, vis,dfsVis)){
+                return true;
+            }
+        }
+    }
+    return false; 
+}
+int main() {
+	int n, m;
+	cin >> n >> m;
+	vector<int> adj[n];
+	for(int i = 0;i<m;i++) {
+	    int u, v;
+	    cin >> u >> v;
+	    adj[u].push_back(v); 
+	    adj[v].push_back(u); 
+	}
+	
+	if(cycleDetect(adj, n)) {
+	    cout << "yes"; 
+	} else {
+	    cout << "No"; 
+	}
+	return 0;
+}
